9ac73737ca581cbaf80b9ce1aad853df1e4236e7
Mohamed Asik Sapteen <ashiq250997@gmail.com>
2024-01-14 21:05:12 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ function About() {
       <p>
         I am passionate about software development and always strive to stay updated with the latest technologies and best practices.
       </p>
+      <p>
+        My experience has helped me gain in-depth knowledge of writing well-designed, testable, and efficient code by using best software development practices. I have gained insights into how the IT industry works. As a software developer here, I've had the opportunity to learn about modern technologies and efficient database operations. Our internal team members' approach to designing optimal code, handling vast amounts of client data, and making the best business decisions kindled a spark within me to explore this dynamic field. I got to deal with members of the payment gateway team, and their ability to anticipate the business impact. My initial function in my team was to create an optimal code based on the client's requirements. Following that, I took responsibility for managing the project on my own, gathering requirements, managing my colleagues, and soon after, I began developing the application, which further inspired me. For cost-cutting reasons, I have managed several clients' databases while designing and developing efficient code to allow multitenancy in the application. My endeavor garnered a lot of appreciation.
+
+        Moreover, I worked on containerizing applications with docker and created a Kubernetes cluster to recover the application in the event of a disaster as well as to maintain replicas of microservices.On top of this I have integrated microservices with keycloak an open source software product to provide role based authentication for specific microservices .Furthermore I utilized Kong API gateway to expose api’s in  a secure manner.In addition, I was in charge of mitigating and resolving the log4j vulnerability in several projects at my company. On the other hand, I'm working on designing apps that employ the MEF standard, which allows telecom-related devices to communicate with numerous partners domains and service domains.
+      </p>
+
     </div>
   );
 }
@@ -65,7 +71,7 @@ function App() {
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
-      setOpen(false); // Close the menu when resizing for a better user experience
+      setOpen(false);
     };
 
     window.addEventListener('resize', handleResize);
@@ -79,28 +85,24 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          {isMobile && (
-            <HamburgerMenu
-              isOpen={isOpen}
-              menuClicked={toggleMenu}
-              width={32}
-              height={24}
-              strokeWidth={3}
-              rotate={0}
-              color="white"
-              borderRadius={0}
-              animationDuration={0.5}
-            />
-          )}
           <nav>
+            {isMobile && (
+              <HamburgerMenu
+                isOpen={isOpen}
+                menuClicked={toggleMenu}
+                width={32}
+                height={24}
+                strokeWidth={3}
+                rotate={0}
+                color="white"
+                borderRadius={0}
+                animationDuration={0.5}
+              />
+            )}
             <ul className={`nav-links ${isOpen && isMobile ? 'open' : ''}`}>
-              {isOpen && isMobile && (
-                <>
-                  <li><NavLink to="/home">Home</NavLink></li>
-                  <li><NavLink to="/about">About</NavLink></li>
-                  <li><NavLink to="/projects">Projects</NavLink></li>
-                </>
-              )}
+              <li><NavLink to="/home">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/projects">Projects</NavLink></li>
             </ul>
           </nav>
         </header>
